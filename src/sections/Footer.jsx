3cc3dbfe9aa2365b7ef366fdab7b005ee378f293
Your Name <you@example.com>
2026-07-21 17:48:50 -0400

    src/sections/Footer.jsx

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -27,7 +27,7 @@ const Footer = () => {
         </div>
         <div className="flex flex-col justify-center">
           <p className="text-center md:text-end">
-            © {new Date().getFullYear()} Dubem Jesse. All rights reserved.
+            © {new Date().getFullYear()} Jesse Odoh. All rights reserved.
           </p>
         </div>
       </div>
